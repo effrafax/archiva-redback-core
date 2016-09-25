1ac222acd2142e95de0398d5127ab17d1cdf704a
Martin Stockhammer <martin_s@apache.org>
2016-09-25 12:25:44 +0200

    Fixing tests after moving persistence.xml

--- redback-users/redback-users-providers/redback-users-jpa/src/test/java/org/apache/archiva/redback/users/jpa/JpaUserManagerTest.java
+++ redback-users/redback-users-providers/redback-users-jpa/src/test/java/org/apache/archiva/redback/users/jpa/JpaUserManagerTest.java
@@ -1,5 +1,24 @@
 package org.apache.archiva.redback.users.jpa;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.archiva.redback.policy.UserSecurityPolicy;
 import org.apache.archiva.redback.users.User;
 import org.apache.archiva.redback.users.UserManager;
@@ -13,6 +32,9 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -36,10 +58,14 @@ public class JpaUserManagerTest extends AbstractUserManagerTestCase {
     public void setUp() throws Exception {
 
         super.setUp();
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("redback-users-jpa");
+        Properties props = new Properties();
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.properties");
+        assert is!=null;
+        props.load(is);
+        is.close();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("redback-jpa",props);
 
         log.info("test setup");
-        Properties props = new Properties();
         jpaUserManager.setEntityManager(emf.createEntityManager());
         super.setUserManager(jpaUserManager);
         assertNotNull(jpaUserManager);
